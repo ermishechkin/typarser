aac1193bb8de5a5e124bf0b31dd7a24f8cc652e3
Aleksandr Ermishechkin <alermishechkin@gmail.com>
2020-10-16 19:45:43 +0300

    Add required option

--- typarser/option.py
+++ typarser/option.py
@@ -5,26 +5,53 @@ from typing import Generic, Literal, Optional, Type, TypeVar, overload
 NAMESPACE = TypeVar('NAMESPACE')
 TYPE = TypeVar('TYPE')
 SELF = TypeVar('SELF', bound='Option')
+RESULT = TypeVar('RESULT')
 
 
-class Option(Generic[TYPE]):
+class Option(Generic[TYPE, RESULT]):
+    # pylint: disable=redefined-builtin
+
+    @overload
+    def __init__(
+        self: Option[TYPE, Optional[TYPE]],
+        *,
+        type: Type[TYPE],
+        required: Literal[False] = False,
+        help: Optional[str] = None,
+    ):
+        ...
+
+    @overload
+    def __init__(
+        self: Option[TYPE, TYPE],
+        *,
+        type: Type[TYPE],
+        required: Literal[True],
+        help: Optional[str] = None,
+    ):
+        ...
+
+    # pylint: enable=redefined-builtin
+
     def __init__(
             self,
             *,
             type: Type[TYPE],  # pylint: disable=redefined-builtin
+            required: bool = False,
             help: Optional[str] = None,  # pylint: disable=redefined-builtin
     ):
         self._type = type
+        self._required = required
         self._help = help
 
     @overload
     def __get__(self: SELF, owner: Literal[None], inst: Type[NAMESPACE]) \
             -> SELF:
-        pass
+        ...
 
     @overload
-    def __get__(self, owner: NAMESPACE, inst: Type[NAMESPACE]) -> TYPE:
-        pass
+    def __get__(self, owner: NAMESPACE, inst: Type[NAMESPACE]) -> RESULT:
+        ...
 
     def __get__(self, owner: Optional[NAMESPACE], inst: Type[NAMESPACE]):
         pass
@@ -39,3 +66,7 @@ class Option(Generic[TYPE]):
     @property
     def help(self) -> Optional[str]:
         return self._help
+
+    @property
+    def required(self) -> bool:
+        return self._required
