0b377f251223fc6ddd73dc0d12c53ae43c1052a1
Aleksandr Ermishechkin <alermishechkin@gmail.com>
2020-11-09 02:04:51 +0300

    Add required option to Commands

--- typarser/command.py
+++ typarser/command.py
@@ -1,18 +1,41 @@
 from __future__ import annotations
 
-from typing import (Generic, Literal, Mapping, Optional, Type, TypeVar, Union,
-                    overload)
+from typing import (Any, Generic, Literal, Mapping, Optional, Type, TypeVar,
+                    Union, overload)
 
 from .namespace import Namespace
 
 NAMESPACE = TypeVar('NAMESPACE', bound=Namespace)
 CMDS = TypeVar('CMDS', bound=Namespace, covariant=True)
+RESULT = TypeVar('RESULT')
 SELF = TypeVar('SELF', bound='Commands')
 
 
-class Commands(Generic[CMDS]):
-    def __init__(self, cmds: Mapping[str, Type[CMDS]]) -> None:
+class Commands(Generic[CMDS, RESULT]):
+    @overload
+    def __init__(
+        self: Commands[CMDS, CMDS],
+        cmds: Mapping[str, Type[CMDS]],
+        *,
+        required: Literal[True],
+    ) -> None:
+        ...
+
+    @overload
+    def __init__(
+        self: Commands[CMDS, Optional[CMDS]],
+        cmds: Mapping[str, Type[CMDS]],
+        *,
+        required: Literal[False] = False,
+    ) -> None:
+        ...
+
+    def __init__(self,
+                 cmds: Mapping[str, Type[CMDS]],
+                 *,
+                 required: bool = False) -> None:
         self._cmds = cmds
+        self._required = required
 
     @overload
     def __get__(self: SELF, owner: Literal[None], inst: Type[NAMESPACE]) \
@@ -21,13 +44,42 @@ class Commands(Generic[CMDS]):
 
     @overload
     def __get__(self, owner: NAMESPACE, inst: Type[NAMESPACE]) \
-            -> CMDS:
+            -> RESULT:
         ...
 
     def __get__(self: SELF, owner: Optional[NAMESPACE],
-                inst: Type[NAMESPACE]) -> Union[SELF, CMDS]:
+                inst: Type[NAMESPACE]) -> Union[SELF, RESULT]:
         raise NotImplementedError
 
     @property
     def entries(self) -> Mapping[str, Type[CMDS]]:
         return self._cmds
+
+    @property
+    def required(self) -> bool:
+        return self._required
+
+    # HACK: __init__ overloading doesn't work correctly for some linters.
+    # Duplicate signatures for __new__ method.
+
+    @overload
+    def __new__(
+        cls,
+        cmds: Mapping[str, Type[CMDS]],
+        *,
+        required: Literal[True],
+    ) -> Commands[CMDS, CMDS]:
+        ...
+
+    @overload
+    def __new__(
+        cls,
+        cmds: Mapping[str, Type[CMDS]],
+        *,
+        required: Literal[False] = False,
+    ) -> Commands[CMDS, Optional[CMDS]]:
+        ...
+
+    def __new__(cls, *args: Any, **kwargs: Any):
+        # pylint: disable=unused-argument
+        return object.__new__(cls)
