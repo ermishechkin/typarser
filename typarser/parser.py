8ccc853ddfee82ee214f96e64faf34c694a39a55
Aleksandr Ermishechkin <alermishechkin@gmail.com>
2020-12-27 04:03:23 +0300

    Add Parser methods for help formatting

--- typarser/parser.py
+++ typarser/parser.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Generic, List, Type
+from typing import IO, Generic, List, Optional, Type
 
 from ._native_parser import ARGS, create_native_parser, parse_args
 
@@ -12,3 +12,15 @@ class Parser(Generic[ARGS]):
 
     def parse(self, args: List[str]) -> ARGS:
         return parse_args(self._native_parser, self._state, args)
+
+    def print_usage(self, file: Optional[IO[str]] = None) -> None:
+        self._native_parser.print_usage(file)
+
+    def print_help(self, file: Optional[IO[str]] = None) -> None:
+        self._native_parser.print_help(file)
+
+    def format_usage(self) -> str:
+        return self._native_parser.format_usage()
+
+    def format_help(self) -> str:
+        return self._native_parser.format_help()
